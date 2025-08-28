28877b046a0ac7a2a97234a3d3c072b1973f6fa8
fylsdy <167668065+fylsdy@users.noreply.github.com>
2025-08-29 03:05:57 +0800

    Create CHDBits.lua

---
+++ CHDBits.lua
@@ -0,0 +1,3 @@
+https://tracker.rainbowisland.co/announce.php?passkey=334bf56454cc17ebd40238a9f26b14b3
+https://chdbits.xyz/announce.php?passkey=334bf56454cc17ebd40238a9f26b14b3
+https://tracker.ptchdbits.co/announce.php?passkey=334bf56454cc17ebd40238a9f26b14b3
